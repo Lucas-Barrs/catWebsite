86b4cc7b22d9e180021daac92a5525d2fb490b00
Lucas Barrs <l.barrs199@gmail.com>
2024-02-26 18:55:39 -0500

    some debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,97 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import RandomPic from './components/RandomPic'
-import SelectBreed from './components/SelectBreed'
-import Gallery from './components/Gallery'
-import { Button, Stack } from '@mui/material'
-import Grid from "@mui/material/Grid";
-import SignUpForm from './components/SignUpForm'
-import Authenticate from "./components/LoginAuth";
-
-
-
+import { useEffect, useState } from 'react';
+import './App.css';
+import RandomPic from './components/RandomPic';
+import SelectBreed from './components/SelectBreed';
+import Gallery from './components/Gallery';
+import { Button, Stack } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import SignUpForm from './components/SignUpForm';
+// import Authenticate from "./components/LoginAuth";
 
 function App() {
-  const [catImgUrl, setCatImgUrl] = useState('')
-  const [selectedBreed, setSelectedBreed] = useState('random')
-  const [favorites, setFavorites] = useState([])
-  const [visibleTab, setVisibleTab] = useState('random')
-  const [token, setToken] = useState(null);
-  
-  useEffect(() => {
-    async function getRandomImage(){
-      const response = await fetch(fetchUrl)
-      const result = await response.json()
-      setCatImgUrl(result[0].url)
-    }
-    getRandomImage()
-  }, [])
+	const [catImgUrl, setCatImgUrl] = useState('');
+	const [selectedBreed, setSelectedBreed] = useState('random');
+	const [favorites, setFavorites] = useState([]);
+	const [visibleTab, setVisibleTab] = useState('random');
+	const [token, setToken] = useState(null);
 
-  const updateImage = async() => {
-    console.log("updateImage function is being called!")
-    const response = await fetch(fetchUrl)
-    const result = await response.json()
-    setCatImgUrl(result[0].url)
-  }
+	useEffect(() => {
+		async function getRandomImage() {
+			const response = await fetch(fetchUrl);
+			const result = await response.json();
+			setCatImgUrl(result[0].url);
+		}
+		getRandomImage();
+	}, []);
 
-  const addToFavorites = (imgUrl) => {
-    const newArray = [...favorites, imgUrl]
-    setFavorites(newArray)
-  }
+	const updateImage = async () => {
+		console.log('updateImage function is being called!');
+		const response = await fetch(fetchUrl);
+		const result = await response.json();
+		setCatImgUrl(result[0].url);
+	};
 
-  console.log(selectedBreed)
-  console.log(favorites)
+	const addToFavorites = (imgUrl) => {
+		const newArray = [...favorites, imgUrl];
+		setFavorites(newArray);
+	};
 
-  const API_URL = `https://api.thecatapi.com/v1/images/search`
-  const queryString = `?breed_ids=${selectedBreed}`
+	console.log(selectedBreed);
+	console.log(favorites);
 
-  const fetchUrl = selectedBreed === 'random'
-    ? API_URL
-    : API_URL + queryString
+	const API_URL = `https://api.thecatapi.com/v1/images/search`;
+	const queryString = `?breed_ids=${selectedBreed}`;
 
-    console.log(fetchUrl)
+	const fetchUrl = selectedBreed === 'random' ? API_URL : API_URL + queryString;
 
-  return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <SelectBreed selectedBreed={selectedBreed} setSelectedBreed={setSelectedBreed} />
-        </Grid>
-        <Grid item  xs={4}>
-          <Stack spacing={2} direction="row">
-            <Button variant="contained" onClick={() => setVisibleTab('random')}>Random</Button>
-            <Button variant="contained" onClick={() => setVisibleTab('favorites')}>Favorites</Button>
-          </Stack>
-        </Grid>
-        <Grid item xs={4}>
-          <SignUpForm setToken={setToken}/>
-          <Authenticate token={token}/>
-        </Grid>
-        <Grid item  xs={8}>
-          <Stack spacing={2} direction="row">
-            <br/><br/>
-          {visibleTab === 'random' && <RandomPic imgUrl={catImgUrl} refetchFunction={updateImage} addToFavorites={addToFavorites}/>}
-          <br/><br/>
-          {visibleTab === 'favorites' && <Gallery favoritesArray={favorites} />}
-          </Stack>
-        </Grid>
-       
-      </Grid>
-    </>
-  )
+	console.log(fetchUrl);
+
+	return (
+		<>
+			<Grid container spacing={2}>
+				<Grid item xs={8}>
+					<SelectBreed
+						selectedBreed={selectedBreed}
+						setSelectedBreed={setSelectedBreed}
+					/>
+				</Grid>
+				<Grid item xs={4}>
+					<Stack spacing={2} direction='row'>
+						<Button variant='contained' onClick={() => setVisibleTab('random')}>
+							Random
+						</Button>
+						<Button
+							variant='contained'
+							onClick={() => setVisibleTab('favorites')}>
+							Favorites
+						</Button>
+					</Stack>
+				</Grid>
+				<Grid item xs={4}>
+					<SignUpForm setToken={setToken} token={token} />
+					{/* <Authenticate token={token}/> */}
+				</Grid>
+				<Grid item xs={8}>
+					<Stack spacing={2} direction='row'>
+						<br />
+						<br />
+						{visibleTab === 'random' && (
+							<RandomPic
+								imgUrl={catImgUrl}
+								refetchFunction={updateImage}
+								addToFavorites={addToFavorites}
+							/>
+						)}
+						<br />
+						<br />
+						{visibleTab === 'favorites' && (
+							<Gallery favoritesArray={favorites} />
+						)}
+					</Stack>
+				</Grid>
+			</Grid>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/RandomPic.jsx
+++ src/components/RandomPic.jsx
@@ -1,19 +1,24 @@
-import { Button } from "@mui/material"
-import './RandomPic.css'
-import { Stack } from "@mui/material"
+import { Button } from '@mui/material';
+import './RandomPic.css';
+import { Stack } from '@mui/material';
 
-
-function RandomPic({imgUrl, addToFavorites, refetchFunction}) {
-  return(
-    <>
-    <Stack  justifyContent="center"
-  alignItems="center" spacing={2} direction="column">
-      <img className="random-pic" src={imgUrl}/>
-      <Button onClick={() => addToFavorites(imgUrl)} variant="outlined" color="success">Add Favorite</Button> 
-      <Button onClick={refetchFunction} variant="outlined" color="success">New Cat</Button>
-    </Stack>
-    </>
-  )
+function RandomPic({ imgUrl, addToFavorites, refetchFunction }) {
+	return (
+		<>
+			<Stack justifyContent='center' spacing={2} direction='column'>
+				<img className='random-pic' src={imgUrl} />
+				<Button
+					onClick={() => addToFavorites(imgUrl)}
+					variant='outlined'
+					color='success'>
+					Add Favorite
+				</Button>
+				<Button onClick={refetchFunction} variant='outlined' color='success'>
+					New Cat
+				</Button>
+			</Stack>
+		</>
+	);
 }
 
-export default RandomPic
+export default RandomPic;
